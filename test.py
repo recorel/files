68756e9b049c232f76bcaa137ff4ca2b0d9f799f
recorel <87754885+recorel@users.noreply.github.com>
2023-05-16 14:28:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,27 +6,27 @@ stack = deque()
 # stack.pop()
 
 
-evaluation = "(1+2) * [4 - 1] + 'ale o co chodzi?'((([[[''' "
+evaluation = '(1+2) * [4 - 1] + \'ale o co chodzi?\'((([[[""" '
 
 
-
-def check(evaluation):
+def check(f_name):
     first = True
-    for i in range(len(evaluation)):
-        ch = evaluation[i]
-        if (ch == '(' or ch == '['):
-            stack.append(ch)
-            continue
-        if (ch == "'" and (first is True)):
-            stack.append(ch)
-            first = False
-            continue
-        if (ch == ')' or ch == ']' or (ch =="'" and first is False)):
-            p = stack.pop()
-            print("Popped: ", p, first)
-            if (ch == "'"):
-                first = True
+    with open(f_name, "r") as f:
+        for line in f:
+            for ch in line:
+                if (ch == '(' or ch == '['):
+                    stack.append(ch)
+                    continue
+                if (ch == '"' and (first is True)):
+                    stack.append(ch)
+                    first = False
+                    continue
+                if (ch == ')' or ch == ']' or (ch =='"' and first is False)):
+                    p = stack.pop()
+                    print("Popped: ", p, first)
+                    if (ch == '"'):
+                        first = True
 
-check(evaluation)
+check("test.txt")
 print(stack)
 
